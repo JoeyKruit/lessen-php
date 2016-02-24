dbdc29e5c2d62469edec90fc92b0ea6f77c1b589
johanstr <jj.strootman@alfa-college.nl>
2016-02-24 10:09:19 +0100

    Test met PDO

--- app/authenticate.php
+++ app/authenticate.php
@@ -1,1 +1,14 @@
 <?php
+
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=les05', 'root', 'root');
+
+    $user_query = $db->prepare('SELECT * FROM users');
+    $user_query->execute();
+    $users = $user_query->fetchAll();
+    var_dump($users);
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+echo '<br />Na de try...catch';
