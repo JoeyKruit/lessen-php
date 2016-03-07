9bb4ae2e4fde3f4b07be733e28c28422f09ec308
johanstr <jj.strootman@alfa-college.nl>
2016-03-07 11:30:28 +0100

    INDEX.PHP voorbereid voor een volgende les

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<pre>
 <?php
 /*
- * LET OP!
- * Dit is nog maar een test
+ * Wat gaan we hier doen?
+ *
+ * TODO     Controleren of iemand ingelogd is
+ * TODO     JA -> Dan gaan we door naar het dashboard
+ * TODO     NEE -> Dan gaan we door naar het login scherm
  */
-    print_r($_REQUEST);
-?>
-</pre>
+session_start();
